55db411983fa0b8c975948ca20c933de06dddf76
Matthew Millns <mmillns@wn3801250.hep.manchester.ac.uk>
2024-11-07 19:40:54 +0000

    cross section has wrong units

--- simulation/crossSection.C
+++ simulation/crossSection.C
@@ -14,15 +14,16 @@ public:
 	enum Constant
 	{
 		pi,
-		muonMass,						// MeV
-		electronMass,				// MeV
-		protonMass,					// MeV
-		neutronMass,				// MeV
-		kaonChargedMass,		// MeV
-		kaon0Mass,			// Mev
+		muonMass,						// GeV
+		electronMass,				// GeV
+		protonMass,					// GeV
+		neutronMass,				// GeV
+		kaonChargedMass,		// GeV
+		kaon0Mass,					// Gev
 		GF,									// GeV^-2
-		fpi,								// MeV
+		fpi,								// GeV
 		Vus,
+		c,									// m/s
 		max_constant
 	};
 
@@ -36,11 +37,11 @@ public:
 			m_p1{ std::move(p1) },
 			m_p2{ std::move(p2) },
 			m_p3{ std::move(p3) },
-			m_ma{ ma }, 
-			m_mb{ mb },
-			m_m1{ m1 },
-			m_m2{ m2 },
-			m_m3{ m3 },
+			m_ma{ ma / (Constant::c*Constant::c) }, 
+			m_mb{ mb / (Constant::c*Constant::c) },
+			m_m1{ m1 / (Constant::c*Constant::c) },
+			m_m2{ m2 / (Constant::c*Constant::c) },
+			m_m3{ m3 / (Constant::c*Constant::c) },
 			m_s{ s },
 			m_ampSquared{ ampSquaredPtr },
 			m_crossSection{ std::numeric_limits<double>::min() },           // -inf => not been calculated
@@ -53,11 +54,11 @@ public:
 			m_p1{ nullptr },
 			m_p2{ nullptr },
 			m_p3{ nullptr },
-			m_ma{ ma }, 
-			m_mb{ mb },
-			m_m1{ m1 },
-			m_m2{ m2 },
-			m_m3{ m3 },
+			m_ma{ ma / (Constant::c*Constant::c) }, 
+			m_mb{ mb / (Constant::c*Constant::c) },
+			m_m1{ m1 / (Constant::c*Constant::c) },
+			m_m2{ m2 / (Constant::c*Constant::c) },
+			m_m3{ m3 / (Constant::c*Constant::c) },
 			m_s{ s },
 			m_ampSquared{ ampSquaredPtr },
 			m_crossSection{ std::numeric_limits<double>::min() },           // -inf => not been calculated
@@ -85,6 +86,7 @@ public:
 public:
 	std::optional<double> getCrossSection() const {return m_crossSection == std::numeric_limits<double>::min() ? std::nullopt :
 																								 std::optional(m_crossSection);}
+	double getBeamEnergy() const {return m_beamEnergy;}
 	static double getConst(Constant c);
 	double crossSectionCalc(int n, int m);
 	void serializeEvent(std::string& out) const;
@@ -129,9 +131,9 @@ private:
 
 double Event::getConst(Constant c)
 {
-	static constexpr std::array<double, static_cast<size_t>(Constant::max_constant)> constants{3.14159265359, 0.1134289259,
-																																	0.51099895000, 938.27208943, 939.57, 493.677, 497.611,
-																																	1.16639e-5, 92.4, 0.22}; 
+	static constexpr std::array<double, static_cast<size_t>(Constant::max_constant)> constants{3.14159265359, 0.1134289259e-3,
+																																	0.51099895000e-3, 938.27208943e-3, 939.57e-3, 493.677e-3,
+																																	497.611e-3, 1.16639e-5, 92.4e-3, 0.22, 299792458}; 
 	return constants[static_cast<size_t>(c)];
 }
 
@@ -150,12 +152,15 @@ double Event::integrand(double E3, double theta, double thetaStar, double phiSta
 {
 	double m12{ m12Calc(m_s, m_m3, E3) };
 	double phaseSpaceFactor{ kallen(m12, m_m1, m_m2) / (m_m1 * m_m2) };
+	//std::cout<<"m12: "<<m12<<", phase space factor: "<<phaseSpaceFactor<<'\n';
+	//std::cout<<"matrix element: "<<ampSquared(theta, thetaStar, phiStar)<<'\n';
 	return std::sqrt(std::pow(E3, 2) - std::pow(m_m3, 2)) * phaseSpaceFactor * ampSquared(theta, thetaStar, phiStar);
 }
 
 double Event::fluxFactor() const
 {
-	double p1Dotp2{ m_p1->dotProduct(*m_p2) };
+	//double p1Dotp2{ m_p1->dotProduct(*m_p2) };
+	double p1Dotp2{ pk() };
 	return 1/(64*std::pow(2*getConst(Constant::pi), 4) * std::sqrt(std::pow(p1Dotp2, 2) - std::pow(m_ma, 2)*std::pow(m_mb, 2)));
 }
 
@@ -212,30 +217,44 @@ double Event::simpsonCalc4D(double a, double b, double c, double d, double e, do
 
 	for(size_t i{0}; static_cast<int>(i) < n; i++)
 	{
+		double x{ a + static_cast<double>(i)*hx };
+		double wx{ static_cast<double>((i == 0 || i == n) ? 1 : (i % 3 == 0 ? 2 : 3)) };
 		for(size_t j{0}; static_cast<int>(j) < m; j++)
 		{
+			double y{ c + static_cast<double>(j)*hy };
+			double wy{ static_cast<double>((j == 0 || j == m) ? 1 : (j % 3 == 0 ? 2 : 3)) };
 			for(size_t k{0}; static_cast<int>(k) < p; k++)
 			{
+				double z{ e + static_cast<double>(k)*hz };
+				double wz{ static_cast<double>((k == 0 || k == p) ? 1 : (k % 3 == 0 ? 2 : 3)) };
 				for(size_t l{0}; static_cast<int>(l) < q; l++)
 				{
-					double x{ a + static_cast<double>(i)*hx };
-					double y{ c + static_cast<double>(j)*hy };
-					double z{ e + static_cast<double>(k)*hz };
-					double w{ g + static_cast<double>(l)*hw };
+					//double x{ a + static_cast<double>(i)*hx };
+					//double y{ c + static_cast<double>(j)*hy };
+					//double z{ e + static_cast<double>(k)*hz };
 
+					double w{ g + static_cast<double>(l)*hw };
+					double ww{ static_cast<double>((l == 0 || l == q) ? 1 : (l % 3 == 0 ? 2 : 3)) };
+					
+					integral += wx * wy * wz * ww * integrand(x, y, z, w);
+
+					/*
 					int weight{ 1 };
 					weightModify(weight, static_cast<int>(i), n);
 					weightModify(weight, static_cast<int>(j), m);
 					weightModify(weight, static_cast<int>(k), p);
 					weightModify(weight, static_cast<int>(l), q);
+					*/
 
-					integral += weight * integrand(x, y, z, w);
+					//integral += weight * integrand(x, y, z, w);
+					//std::cout<<"weight: "<<weight<<", Integrand: "<<integrand(x, y, z, w)<<'\n';
 				}
 			}
 		}
 	}
 
-	integral *= (3/8) * hx * hy * hz * hw;
+	integral *= (3.0*hx/8.0) * (3.0*hy/8.0) * (3.0*hz/8.0) * (3.0*hw/8.0);
+	//std::cout<<"Integral: "<<integral<<'\n';
 	return integral;
 }
 
@@ -293,32 +312,32 @@ double Event::qSquared(double theta, double thetaStar, double phiStar) const
 
 double Event::pk() const
 {
-	return 0.5*(m_s - m_mb*m_mb - m_ma*m_ma);
+	return 0.5*(m_s - m_mb*m_mb - m_ma*m_ma) / (Constant::c*Constant::c);
 }
 
 double Event::ppPrime(double theta, double thetaStar, double phiStar) const
 {
-	return 0.5*(-qSquared(theta, thetaStar, phiStar) + 2*m_ma*m_ma);
+	return 0.5*(-qSquared(theta, thetaStar, phiStar) + 2*m_ma*m_ma) / (Constant::c*Constant::c);
 }
 
 double Event::pkPrime(double theta, double thetaStar, double phiStar) const
 {
-	return 0.5*(m_s + qSquared(theta, thetaStar, phiStar) - m_ma*m_ma - m_mb*m_mb);
+	return 0.5*(m_s + qSquared(theta, thetaStar, phiStar) - m_ma*m_ma - m_mb*m_mb) / (Constant::c*Constant::c);
 }
 
 double Event::pPrimekPrime() const
 {
-	return 0.5*(m_s - m_ma*m_ma - m_mb*m_mb);
+	return 0.5*(m_s - m_ma*m_ma - m_mb*m_mb) / (Constant::c*Constant::c);
 }
 
 double Event::kkPrime(double theta, double thetaStar, double phiStar) const
 {
-	return 0.5*(-qSquared(theta, thetaStar, phiStar) + 2*m_mb*m_mb);
+	return 0.5*(-qSquared(theta, thetaStar, phiStar) + 2*m_mb*m_mb) / (Constant::c*Constant::c);
 }
 
 double Event::pPrimek(double theta, double thetaStar, double phiStar) const
 {
-	return 0.5*(m_s + qSquared(theta, thetaStar, phiStar) - m_ma*m_ma - m_mb*m_mb);
+	return 0.5*(m_s + qSquared(theta, thetaStar, phiStar) - m_ma*m_ma - m_mb*m_mb) / (Constant::c*Constant::c);
 }
 
 double Event::ampSquared(double theta, double thetaStar, double phiStar) const
@@ -329,7 +348,14 @@ double Event::ampSquared(double theta, double thetaStar, double phiStar) const
 	double d{ pPrimekPrime() };
 	double e{ kkPrime(theta, thetaStar, phiStar) };
 	double f{ pPrimek(theta, thetaStar, phiStar) };
-	return m_ampSquared(a, b, c, d, e, f);
+	//std::cout<<a<<", "<<b<<", "<<c<<", "<<d<<", "<<e<<", "<<f<<'\n';
+	if(m_ampSquared)
+	{
+		//std::cout<<m_ampSquared(a, b, c, d, e, f)<<'\n';
+		return m_ampSquared(a, b, c, d, e, f);
+	}
+	else
+		return 0;
 }
 
 
@@ -337,11 +363,12 @@ namespace mat
 {
 	namespace con
 	{
-		double D{ 0.804 };
-		double F{ 0.463 };
-		double GF{ 1.16639e-5 };
-		double fPi{ 92.4 };
-		double Vus{ 0.22 };
+		constexpr double D{ 0.804 };
+		constexpr double F{ 0.463 };
+		constexpr double GF{ 1.16639e-5 };
+		constexpr double fPi{ 92.4e-3 };
+		constexpr double Vus{ 0.22 };
+		constexpr double c{ 299792458 };
 	}
 	
 	//      a   b    c    d     e    f
@@ -349,11 +376,15 @@ namespace mat
 
 	double contactTerm(double a, double b, double c, double d, double e, double f)
 	{
-		double ACT{ 1 };
-		double BCT{ con::D - con::F };
-		double m1{ Event::getConst(Event::neutronMass) };		// neutron -> neutron
-		double m2{ Event::getConst(Event::protonMass) };
-		double front{ (1/16) * con::GF*con::GF * ACT*ACT * con::Vus*con::Vus * (1/(con::fPi*con::fPi)) };
+		constexpr double ACT{ 1 };
+		constexpr double BCT{ con::D - con::F };
+		double m1{ Event::getConst(Event::neutronMass) / (con::c*con::c) };		// neutron -> neutron
+	  double m2{ Event::getConst(Event::protonMass) / (con::c*con::c) };
+		//std::cout<<ACT<<", "<<BCT<<", "<<m1<<", "<<m2<<'\n';
+		//std::cout<<con::GF*con::GF<<", "<<con::fPi*con::fPi<<", "<<con::Vus*con::Vus<<'\n';
+		//std::cout<<1/((con::fPi)*(con::fPi))<<", ";
+		double front{ 0.0625 * (con::GF)*(con::GF) * ACT*ACT * (con::Vus)*(con::Vus) * (1/((con::fPi)*(con::fPi))) };
+		//std::cout<<"front: "<<front<<'\n';
 		return front * ( 64*(a*d + f*c) + 64*BCT*(a*d + c*f + b*e - 3*(e*(b + m1*m2))) );
 	}
 }
@@ -387,6 +418,7 @@ void crossSection()
 	auto makeEvent2 = [&id](double ma, double mb, double m1, double m2, double m3, double s, 
 													double (*ampSquared)(double, double, double, double, double, double))
 	{
+
 		Event e{id, ma, mb, m1, m2, m3, s, ampSquared};
 		id++;
 		return e;
@@ -396,12 +428,41 @@ void crossSection()
 	// CT electron neutrino	+ neutron -> proton + electron, s = 3.25GeV^2	
 	double (*func)(double, double, double, double, double, double);
 	func = mat::contactTerm;
+	/*
 	Event e = makeEvent2(Event::getConst(Event::neutronMass), 0, Event::getConst(Event::protonMass), 
 											 Event::getConst(Event::electronMass), Event::getConst(Event::kaon0Mass), 3.25, func);
-	std::cout<<"Cross Section: "<<e.crossSectionCalc(100, 20)<<'\n';
+	//std::cout<<"Cross Section: "<<e.crossSectionCalc(100, 20)<<'\n';
+	e.crossSectionCalc(100, 20);
 	if(auto cs = e.getCrossSection())
 	{
-		std::cout<<"Cross Section: "<<*cs<<'\n';
+		std::cout<<"Cross Section: "<<*cs<<", Beam Energy: "<<e.getBeamEnergy()<<'\n';
 	}
+	*/
+	double sMin{ 0.9 };
+	double sMax{ 4.88 };
+	constexpr size_t N{ 50 };
+	double step{ (sMax - sMin) / N };
+	std::array<double, N> crossSections;
+	std::array<double, N> beamEnergies;
+	std::string data;
+	for(size_t i{ 0 }; i < N; i++)
+	{
+		double s{ sMin + i*step };
+		Event e = makeEvent2(Event::getConst(Event::neutronMass), 0, Event::getConst(Event::protonMass), 
+											 Event::getConst(Event::electronMass), Event::getConst(Event::kaon0Mass), s, func);
+		//std::cout<<"made event\n";
+		e.crossSectionCalc(100, 20);
+		if(auto cs = e.getCrossSection())
+		{
+			//std::cout<<"here\n";
+			//crossSections[i] = *cs;
+			beamEnergies[i] = e.getBeamEnergy();
+			e.serializeEvent(data);
+		}		
+	}
+	
 
+
+	//std::cout<<data.size();
+	//std::cout<<data<<'\n';
 }
