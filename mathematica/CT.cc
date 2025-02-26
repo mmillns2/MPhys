90effbe145eb9f035445422dc2f288fefa7432a2
Oliver Gregory <olwgregory@gmail.com>
2025-02-26 17:32:44 +0000

    fixed CT.nb and CT.cc properly

--- mathematica/CT.cc
+++ mathematica/CT.cc
@@ -1,50 +1,22 @@
-(Complex(0,64)*Power(ACT,2)*Power(Vus,2)*
-      Eps(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) - (Complex(0,64)*Power(ACT,2)*Power(BCT,2)*
-      Power(Vus,2)*Eps(Momentum(p1,ma,m1,m2,m3,s,W,theta,
-        thetaStar,phiStar),
-       Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) + (Complex(0,64)*Power(ACT,2)*Power(Vus,2)*
-      Eps(Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) - (Complex(0,64)*Power(ACT,2)*Power(BCT,2)*
-      Power(Vus,2)*Eps(Momentum(q1,ma,m1,m2,m3,s,W,theta,
-        thetaStar,phiStar),
-       Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) + (128*Power(ACT,2)*Power(Vus,2)*
-      Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
+(64*Power(ACT,2)*Power(Vus,2)*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
        Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-      Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) - (128*Power(ACT,2)*Power(BCT,2)*Power(Vus,2)*
-      Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
+      Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/Power(fPi,2) + 
+   (128*Power(ACT,2)*BCT*Power(Vus,2)*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
        Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-      Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) - (128*Power(ACT,2)*m1*ma*Power(Vus,2)*
-      Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) - (128*Power(ACT,2)*Power(BCT,2)*m1*ma*
-      Power(Vus,2)*Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,
-        thetaStar,phiStar),
-       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) + (128*Power(ACT,2)*Power(Vus,2)*
-      Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
+      Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/Power(fPi,2) + 
+   (64*Power(ACT,2)*Power(BCT,2)*Power(Vus,2)*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
+       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
+      Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/Power(fPi,2) - 
+   (64*Power(ACT,2)*m1*ma*Power(Vus,2)*Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
+       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/Power(fPi,2) + 
+   (64*Power(ACT,2)*Power(BCT,2)*m1*ma*Power(Vus,2)*Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
+       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/Power(fPi,2) + 
+   (64*Power(ACT,2)*Power(Vus,2)*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
        Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-      Pair(Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2) - (128*Power(ACT,2)*Power(BCT,2)*Power(Vus,2)*
-      Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
+      Pair(Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/Power(fPi,2) - 
+   (128*Power(ACT,2)*BCT*Power(Vus,2)*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
        Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-      Pair(Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
-       Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/
-    Power(fPi,2)
+      Pair(Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/Power(fPi,2) + 
+   (64*Power(ACT,2)*Power(BCT,2)*Power(Vus,2)*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),
+       Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
+      Pair(Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))/Power(fPi,2)
