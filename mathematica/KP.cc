e41633f05133de757abcb8c425c5695c96b4e1ab
Oliver Gregory <olwgregory@gmail.com>
2025-02-28 12:31:04 +0000

    Adding Kaon Pole term .nb and .cc

---
+++ mathematica/KP.cc
@@ -0,0 +1,14 @@
+(2*Power(AKP,2)*Power(Vus,2)*(2*m1*ma*Pair(Momentum(q),Momentum(q2)) + m1*ma*Pair(Momentum(q2),Momentum(q2)) + 
+       2*Pair(Momentum(q),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*Pair(Momentum(q2),Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) + 
+       2*Pair(Momentum(q2),Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*Pair(Momentum(q2),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) + 
+       2*Pair(Momentum(q),Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
+        (Pair(Momentum(q),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) + Pair(Momentum(q2),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))) + 
+       Pair(Momentum(q),Momentum(q))*(m1*ma - Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))) - 
+       2*Pair(Momentum(q),Momentum(q2))*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) - 
+       Pair(Momentum(q2),Momentum(q2))*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))*
+     (Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
+        (Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) - 
+          2*Pair(Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))) + 
+       Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
+        Pair(Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))))/
+   (Power(fPi,2)*Power(Power(m2,2) - Power(q,2),2))
