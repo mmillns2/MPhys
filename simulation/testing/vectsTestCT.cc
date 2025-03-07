4cb45cddb9c85180149dcd27285d9444bd469ec0
Oliver Gregory <olwgregory@gmail.com>
2025-03-07 13:47:45 +0000

    added a complete antiCRSigma notebook and updated the vectsTest files

--- simulation/testing/vectsTestCT.cc
+++ simulation/testing/vectsTestCT.cc
@@ -1,8 +1,12 @@
-(64*Power(ACT,2)*Power(Vus,2)*Pair(Momentum(p1,vects),Momentum(q3,vects))*Pair(Momentum(p2,vects),Momentum(q1,vects)))/Power(fPi,2) + 
-   (128*Power(ACT,2)*BCT*Power(Vus,2)*Pair(Momentum(p1,vects),Momentum(q3,vects))*Pair(Momentum(p2,vects),Momentum(q1,vects)))/Power(fPi,2) + 
-   (64*Power(ACT,2)*Power(BCT,2)*Power(Vus,2)*Pair(Momentum(p1,vects),Momentum(q3,vects))*Pair(Momentum(p2,vects),Momentum(q1,vects)))/Power(fPi,2) - 
-   (64*Power(ACT,2)*m1*ma*Power(Vus,2)*Pair(Momentum(p2,vects),Momentum(q3,vects)))/Power(fPi,2) + 
-   (64*Power(ACT,2)*Power(BCT,2)*m1*ma*Power(Vus,2)*Pair(Momentum(p2,vects),Momentum(q3,vects)))/Power(fPi,2) + 
-   (64*Power(ACT,2)*Power(Vus,2)*Pair(Momentum(p1,vects),Momentum(p2,vects))*Pair(Momentum(q1,vects),Momentum(q3,vects)))/Power(fPi,2) - 
-   (128*Power(ACT,2)*BCT*Power(Vus,2)*Pair(Momentum(p1,vects),Momentum(p2,vects))*Pair(Momentum(q1,vects),Momentum(q3,vects)))/Power(fPi,2) + 
-   (64*Power(ACT,2)*Power(BCT,2)*Power(Vus,2)*Pair(Momentum(p1,vects),Momentum(p2,vects))*Pair(Momentum(q1,vects),Momentum(q3,vects)))/Power(fPi,2)
+(64*Power(ACT,2)*Power(Vus,2)*Pair(Momentum(p1,m1,m2,m3,vects),Momentum(q3,m1,m2,m3,vects))*Pair(Momentum(p2,m1,m2,m3,vects),Momentum(q1,m1,m2,m3,vects)))/
+    Power(fPi,2) + (128*Power(ACT,2)*BCT*Power(Vus,2)*Pair(Momentum(p1,m1,m2,m3,vects),Momentum(q3,m1,m2,m3,vects))*
+      Pair(Momentum(p2,m1,m2,m3,vects),Momentum(q1,m1,m2,m3,vects)))/Power(fPi,2) + 
+   (64*Power(ACT,2)*Power(BCT,2)*Power(Vus,2)*Pair(Momentum(p1,m1,m2,m3,vects),Momentum(q3,m1,m2,m3,vects))*
+      Pair(Momentum(p2,m1,m2,m3,vects),Momentum(q1,m1,m2,m3,vects)))/Power(fPi,2) - 
+   (64*Power(ACT,2)*m1*ma*Power(Vus,2)*Pair(Momentum(p2,m1,m2,m3,vects),Momentum(q3,m1,m2,m3,vects)))/Power(fPi,2) + 
+   (64*Power(ACT,2)*Power(BCT,2)*m1*ma*Power(Vus,2)*Pair(Momentum(p2,m1,m2,m3,vects),Momentum(q3,m1,m2,m3,vects)))/Power(fPi,2) + 
+   (64*Power(ACT,2)*Power(Vus,2)*Pair(Momentum(p1,m1,m2,m3,vects),Momentum(p2,m1,m2,m3,vects))*Pair(Momentum(q1,m1,m2,m3,vects),Momentum(q3,m1,m2,m3,vects)))/
+    Power(fPi,2) - (128*Power(ACT,2)*BCT*Power(Vus,2)*Pair(Momentum(p1,m1,m2,m3,vects),Momentum(p2,m1,m2,m3,vects))*
+      Pair(Momentum(q1,m1,m2,m3,vects),Momentum(q3,m1,m2,m3,vects)))/Power(fPi,2) + 
+   (64*Power(ACT,2)*Power(BCT,2)*Power(Vus,2)*Pair(Momentum(p1,m1,m2,m3,vects),Momentum(p2,m1,m2,m3,vects))*
+      Pair(Momentum(q1,m1,m2,m3,vects),Momentum(q3,m1,m2,m3,vects)))/Power(fPi,2)
