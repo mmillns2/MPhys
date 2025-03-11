fb3863cb2e7f81269ce06b74d098fcffd576a184
Oliver Gregory <olwgregory@gmail.com>
2025-03-11 11:31:31 +0000

    KP final form: notebook and matrix element

--- simulation/matrixElementFiles/KP.cc
+++ simulation/matrixElementFiles/KP.cc
@@ -1,21 +1,8 @@
-(2*Power(AKP,2)*Power(Vus,2)*((m1*ma - Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))*
-        Pair(Momentum(q,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) + 
-       2*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-        Pair(Momentum(q,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) + 
-       2*m1*ma*Pair(Momentum(q,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) - 
-       2*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-        Pair(Momentum(q,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) + 
-       2*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-        Pair(Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) + 
-       2*Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-        (Pair(Momentum(q,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) + 
-          Pair(Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))) + 
-       m1*ma*Pair(Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) - 
-       Pair(Momentum(p1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q1,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-        Pair(Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)))*
-     (Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-        (Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar)) - 
-          2*Pair(Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))) + 
-       Pair(Momentum(p2,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))*
-        Pair(Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar),Momentum(q3,ma,m1,m2,m3,s,W,theta,thetaStar,phiStar))))/
-   (Power(fPi,2)*Power(Power(m2,2) - Power(q,2),2))
+(2*Power(AKP,2)*Power(Vus,2)*((m1*ma - Pair(Momentum(p1,vects),Momentum(q1,vects)))*Pair(Momentum(q,vects),Momentum(q,vects)) + 
+       2*Pair(Momentum(p1,vects),Momentum(q2,vects))*Pair(Momentum(q,vects),Momentum(q1,vects)) + 2*m1*ma*Pair(Momentum(q,vects),Momentum(q2,vects)) - 
+       2*Pair(Momentum(p1,vects),Momentum(q1,vects))*Pair(Momentum(q,vects),Momentum(q2,vects)) + 
+       2*Pair(Momentum(p1,vects),Momentum(q2,vects))*Pair(Momentum(q1,vects),Momentum(q2,vects)) + 
+       2*Pair(Momentum(p1,vects),Momentum(q,vects))*(Pair(Momentum(q,vects),Momentum(q1,vects)) + Pair(Momentum(q1,vects),Momentum(q2,vects))) + 
+       m1*ma*Pair(Momentum(q2,vects),Momentum(q2,vects)) - Pair(Momentum(p1,vects),Momentum(q1,vects))*Pair(Momentum(q2,vects),Momentum(q2,vects)))*
+     (Pair(Momentum(p2,vects),Momentum(p2,vects))*(Pair(Momentum(p2,vects),Momentum(q3,vects)) - 2*Pair(Momentum(q3,vects),Momentum(q3,vects))) + 
+       Pair(Momentum(p2,vects),Momentum(q3,vects))*Pair(Momentum(q3,vects),Momentum(q3,vects))))/(Power(fPi,2)*Power(Power(m2,2) - Power(q,2),2))
